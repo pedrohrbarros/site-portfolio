9a3820410dc5a14a7d355ccbae25a446eda99c46
pedrohrbarros <pedrobarros232@hotmail.com>
2023-06-09 16:52:48 -0300

    feat: add views for person app

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -95,3 +95,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ]
+}
--- backend/person/views.py
+++ backend/person/views.py
@@ -1,1 +1,24 @@
-from django.shortcuts import render
+from .models import Person, Role, Project, Work
+from .serializer import PersonSerializer, RoleSerializer, ProjectSerializer, WorkSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class PersonView(APIView):
+  def get(self, request, format='json'):
+    persons = [person for person in Person.objects.all()]
+    return Response(persons)
+
+class ProjectView(APIView):
+  def get(self, request, format='json'):
+    projects = [project for project in Project.objects.all()]
+    return Response(project)
+
+class RoleView(APIView):
+  def get(self, request, format='json'):
+    roles = [role for role in Role.objects.all()]
+    return Response(roles)
+
+class WorkView(APIView):
+  def get(self, request, format='json'):
+    works = [work for work in Work.objects.all()]
+    return Response(works)
