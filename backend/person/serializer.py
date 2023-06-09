d1e1b96e58757f7873528541df7a00122abe8cc5
pedrohrbarros <pedrobarros232@hotmail.com>
2023-06-09 16:44:42 -0300

    feat: add person serializer

--- backend/person/serializer.py
+++ backend/person/serializer.py
@@ -2,4 +2,17 @@ from rest_framework import serializers
 from .models import Person, Role, Project, Work
 
 class PersonSerializer(serializers.ModelSerializer):
-  
+  class Meta:
+    model = Person
+    
+class RoleSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Role
+
+class ProjectSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Project
+
+class WorkSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Work
