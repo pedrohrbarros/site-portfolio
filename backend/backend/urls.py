16720553d2852782268e3bb39f9f9ad266edd7a6
pedrohrbarros <pedrobarros232@hotmail.com>
2023-06-13 13:10:30 -0300

    fix: removing url impot from backend.urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import url
 from django.views.static import serve
 from django.conf.urls.static import static
 from django.conf import settings
@@ -13,9 +12,9 @@ urlpatterns = [
     re_path(r'^%s(?P<path>.*)$' % re.escape(settings.STATIC_URL.lstrip('/')), serve, {"document_root": settings.STATIC_ROOT}),
 ]
 
-url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+
 admin.site.site_header = 'Site Portfolio'
 admin.site.index_title = 'Site Portfolio Admin Home'
 admin.site.site_title = 'Site Portfolio'
