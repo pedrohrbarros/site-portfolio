bd239875c1202339fa66eed47c1c234070bc3818
pedrohrbarros <pedrobarros232@hotmail.com>
2023-06-13 12:33:36 -0300

    feat: add staticfiles

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('person.urls'))
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 admin.site.site_header = 'Site Portfolio'
 admin.site.index_title = 'Site Portfolio Admin Home'
